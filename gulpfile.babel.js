acd41065ccadb5248b90eb18114bc4b545d68493
CGJI58 <leemsk22@naver.com>
2023-05-18 13:28:08 +0900

    webserver with live reload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,36 @@
 import gulp from "gulp";
 import gulpPug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
 };
 
-const pug = () => {
+const handlePug = () => {
   return gulp
-    .src(routes.pug.src)
+    .src(routes.pug.src) // 새로고침 되려면 index.pug 저장을 눌러야만 함
     .pipe(gulpPug())
     .pipe(gulp.dest(routes.pug.dest));
 };
 
 const clean = () => del([routes.pug.dest]);
 
+const webserver = () =>
+  gulp.src(routes.pug.dest).pipe(ws({ livereload: true, open: true }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, handlePug);
+};
+
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([handlePug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
