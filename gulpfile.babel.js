75005e11635f7d9e68934524c71acc6b9025b452
CGJI58 <leemsk22@naver.com>
2023-05-18 14:48:16 +0900

    gulpsass and dartsass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,10 @@ import gulp from "gulp";
 import gulpPug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+
+const sass = gulpSass(dartSass);
 
 const routes = {
   pug: {
@@ -9,14 +13,25 @@ const routes = {
     src: "src/*.pug",
     dest: "build",
   },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css/",
+  },
 };
 
-const handlePug = () => {
+const handlePugs = () => {
   return gulp
     .src(routes.pug.src) // 새로고침 되려면 index.pug 저장을 눌러야만 함
     .pipe(gulpPug())
     .pipe(gulp.dest(routes.pug.dest));
 };
+
+const handleStyles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
 
 const clean = () => del([routes.pug.dest]);
 
@@ -24,12 +39,13 @@ const webserver = () =>
   gulp.src(routes.pug.dest).pipe(ws({ livereload: true, open: true }));
 
 const watch = () => {
-  gulp.watch(routes.pug.watch, handlePug);
+  gulp.watch(routes.pug.watch, handlePugs);
+  gulp.watch(routes.scss.watch, handleStyles);
 };
 
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([handlePug]);
+const assets = gulp.series([handlePugs, handleStyles]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
